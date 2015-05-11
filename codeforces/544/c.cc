d0bad95e24a1571047d04de3525433afd48c87e1
Justin Ting <justingling@gmail.com>
2015-05-11 23:22:00 +1000

    more failed attempts at 544c

--- codeforces/544/c.cc
+++ codeforces/544/c.cc
@@ -2,6 +2,13 @@
 #include <cstring>
 using namespace std;
 
+int sum(int size, int *arr)
+{
+	int total = 0;
+	for (int i = 0; i < size; ++i) total += arr[i];
+	return total;
+}
+
 int main()
 {
 	int prog_c, line_c, err_c, mod;
@@ -9,12 +16,6 @@ int main()
 	int err_l[prog_c];
 	for (int i = 0; i < prog_c; ++i) cin >> err_l[i];
 
-	int perms[prog_c];
-	int distinct_c = 0;
-	memset(perms, 0, sizeof(perms));
-	perms[prog_c=1] = line_c;
-	while (perms[0] != line_c)
-	{
-					
-	}
+	
+		
 }
